502fdfb1bfd29138533a8250909ec3d2110d2fcb
Prakarshprasar <prakprasar@gmail.com>
2022-10-14 16:02:37 +0530

    auth

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -6,65 +6,73 @@ const userRouter=Router();
 
 userRouter.post("/:id",async(req,res)=>{
     let username=req.params.id;
-
-    axios.get(`https://api.github.com/users/${username}`)
-    .then(async(response)=>{
-        
-        
-        let arr1=[];
-        let arr2=[]
-        axios.get(response.data.followers_url).then((r)=>{
-            arr1= r.data;
-            let url2=response.data.following_url.trim().split("");
-            for(let i=0;i<13;i++){
-                url2.pop()
-            }
-            let url=url2.join("")
+    axios.get("http://localhost:8080/git/users").then((resloc)=>{
+        axios.get(`https://api.github.com/users/${username}`)
+        .then(async(response)=>{
             
-            axios.get(url).then(async(r)=>{
-                arr2=r.data;
-                let arr=[];
-                if(arr1.length<arr2.length){
-                    for(let i=0;i<arr1.length;i++){
-                        for(let j=0;j<arr2.length;j++){
-                            if(arr1[i].login==arr2[j].login){
-                                arr.push(arr1[i].login);
-                                break;
-                            }
-                        }
-                    }
-                }else{
-                    for(let i=0;i<arr2.length;i++){
-                        for(let j=0;j<arr1.length;j++){
-                            if(arr2[i].login==arr1[j].login){
-                                arr.push(arr2[i].login);
-                                break;
-                            }
-                        }
-                    }
+            let d=resloc.data;
+            // let c=0;
+            for(let i=0;i<d.length;i++){
+                if(d[i].login==username){
+                    return res.send("user already exists")
                 }
+            }
+            let arr1=[];
+            let arr2=[]
+            axios.get(response.data.followers_url).then((r)=>{
+                arr1= r.data;
+                let url2=response.data.following_url.trim().split("");
+                for(let i=0;i<13;i++){
+                    url2.pop()
+                }
+                let url=url2.join("")
                 
-               
-                const user=await new User(response.data)
-                user.friends=arr;
-                user.save()
-                return res.send(user)
+                axios.get(url).then(async(r)=>{
+                    arr2=r.data;
+                    let arr=[];
+                    if(arr1.length<arr2.length){
+                        for(let i=0;i<arr1.length;i++){
+                            for(let j=0;j<arr2.length;j++){
+                                if(arr1[i].login==arr2[j].login){
+                                    arr.push(arr1[i].login);
+                                    break;
+                                }
+                            }
+                        }
+                    }else{
+                        for(let i=0;i<arr2.length;i++){
+                            for(let j=0;j<arr1.length;j++){
+                                if(arr2[i].login==arr1[j].login){
+                                    arr.push(arr2[i].login);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    
+                   
+                    const user=await new User(response.data)
+                    user.friends=arr;
+                    user.save()
+                    return res.send(user)
+                    
+                })
                 
             })
             
+    
+            // console.log(arr1,arr2)
+            
+            
+            
+            
         })
-        
-
-        // console.log(arr1,arr2)
-        
-        
-        
-        
+        .catch((err)=>{
+            console.log("error occured")
+            return res.send("error recieved")
+        })
     })
-    .catch((err)=>{
-        console.log("error occured")
-        return res.send("error recieved")
-    })
+    
 })
 userRouter.get("/users",async(req,res)=>{
 
